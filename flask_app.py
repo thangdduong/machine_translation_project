c4c600e498b1a16526d0ae6f67b159f3575818c9
thangdduong <19522195@gm.uit.edu.vn>
2022-12-29 17:31:01 +0700

    rmeove app run

--- flask_app.py
+++ flask_app.py
@@ -28,5 +28,5 @@ def translation_api():
     else:
         return ("nothing")
     
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+# if __name__ == "__main__":
+#     app.run(host='127.0.0.1', port=8080, debug=True)
