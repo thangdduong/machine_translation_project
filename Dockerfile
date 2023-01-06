50b5628c28f8818a0b1468acc0efd484765765f9
thangdduong <19522195@gm.uit.edu.vn>
2023-01-06 18:00:32 +0700

    change deploy address

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY . /app/
 
 RUN pip install -r requirements.txt
 
-CMD ["gunicorn", "app:app"]
+EXPOSE 8080
+
+CMD ["python3", "app.py"]
--- app.py
+++ app.py
@@ -28,5 +28,8 @@ def translation_api():
     else:
         return ("nothing")
     
-# if __name__ == "__main__":
-#     app.run(host='127.0.0.1', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', 
+            port=8080, 
+            debug=True
+    )
